f110337cf58042564064e915cdf16bbde2da9016
Acarphia <76790334+Acarphia@users.noreply.github.com>
2025-03-30 19:36:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -37,11 +37,25 @@ function sendMessage() {
     } else {
         // Respostas para as opções depois do CPF
         if (message === "1") {
-            displayMessage("Escolha uma opção relacionada ao embarque da carga:\n1 - Local e responsável pelo embarque\n2 - Tipo de carga\n3 - Registro fotográfico da carga\n4 - KM inicial registrado", "bot-message");
+            displayMessage("Escolha uma opção relacionada ao embarque da carga:\n1 - Local e responsável pelo embarque\n2 - Tipo de carga\n3 - Registro fotográfico da carga no embarque\n4 - KM inicial registrado", "bot-message");
+            if (message === "1") { // Local e responsável pelo embarque
+                const { embarqueLocal, embarqueResponsavel } = usersData[cpf];
+                displayMessage(`O seu local para embarque é ${embarqueLocal} e o responsável pelo embarque é ${embarqueResponsavel}.`, "bot-message");
+            } else if (message === "2") { // Tipo de carga
+                const { tipoCarga } = usersData[cpf];
+                displayMessage(`O seu tipo de carga é ${tipoCarga}.`, "bot-message");
+            } else if (message === "3") { // Registro fotográfico da carga no embarque
+            }
+            } else if (message === "4") { // KM inicial registrado
+            }
         } else if (message === "2") {
             displayMessage("Escolha uma opção relacionada à rota da viagem:\n1 - Melhor caminho e condições das estradas\n2 - Paradas para descanso, alimentação e abastecimento\n3 - Viagem pré-registrada no GPS\n4 - Observações sobre a carga\n5 - Registro de custos", "bot-message");
         } else if (message === "3") {
-            displayMessage("Escolha uma opção relacionada ao desembarque da carga:\n1 - Local e responsável pelo desembarque\n2 - Registro fotográfico no desembarque\n3 - KM final registrado", "bot-message");
+            displayMessage("Escolha uma opção relacionada ao desembarque da carga:\n1 - Local e responsável pelo desembarque\n2 - Registro fotográfico da carga no desembarque\n3 - KM final registrado", "bot-message");
+            if (message === "1") { // Local e responsável pelo desembarque
+                const { desembarqueLocal, desembarqueResponsavel } = usersData[cpf];
+                displayMessage(`O local de desembarque é ${desembarqueLocal} e o responsável pelo desembarque é ${desembarqueResponsavel}.`, "bot-message");
+    } else if (message === "2") { // Registro fotográfico no desembarque
         } else if (message === "4") {
             displayMessage("Para questões pós-viagem, mande mensagem para +55 34 9894-2493.", "bot-message");
         } else if (message === "5") {
