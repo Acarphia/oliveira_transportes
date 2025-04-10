6f6e4b6e1e3661ebc0eca3124bd8a9886aff3778
Acarphia <76790334+Acarphia@users.noreply.github.com>
2025-04-10 00:15:45 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'Oliveira-Transportes-v14'; // Altere a versão sempre que atualizar
+const CACHE_NAME = 'Oliveira-Transportes-v15'; // Altere a versão sempre que atualizar
 const urlsToCache = [
   '/',
   '/index.html',
